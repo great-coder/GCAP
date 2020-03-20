49748762092b701bd56dabd77a5bb3cc5ab1ebc9
Coder the Great <m_dehghan9112@outlook.com>
2020-03-21 02:34:10 +0430

    Make app to use Serilog

--- GCAP/Program.cs
+++ GCAP/Program.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace GCAP
 {
@@ -13,6 +11,7 @@ namespace GCAP
     {
         public static void Main(string[] args)
         {
+            Console.Title = "GCAPv1.0";
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,6 +20,16 @@ namespace GCAP
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog((context, configuration) =>
+                    {
+                        configuration
+                            .MinimumLevel.Debug()
+                            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                            .MinimumLevel.Override("System", LogEventLevel.Warning)
+                            .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                            .Enrich.FromLogContext()
+                            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate);
+                    });
                 });
     }
 }
