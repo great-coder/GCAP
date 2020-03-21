e2e935caebd3b77169c3a61c9ada5bee71995ba0
Coder the Great <m_dehghan9112@outlook.com>
2020-03-21 12:48:57 +0430

    Identity api as an endpoint on accessing resource

---
+++ GCAP.API/Controllers/IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCAP.API.Controllers
+{
+    public class IdentityController : ControllerBase
+    {
+        [Route("identity")]
+        [Authorize]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
