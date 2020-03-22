5fc233cde4b1e4c664297eeed613b876ef9b7913
Coder the Great <m_dehghan9112@outlook.com>
2020-03-23 02:27:39 +0430

    Adding mvc client for gcap server

---
+++ GCAP.ClientMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GCAP.ClientMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
