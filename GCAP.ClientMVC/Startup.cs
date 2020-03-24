c76acc434e17ea71b1053b41bcae7c6ebbb9d967
Coder the Great <m_dehghan9112@outlook.com>
2020-03-24 19:41:55 +0430

    Resolving problem of login and logout redirection

--- GCAP.ClientMVC/Startup.cs
+++ GCAP.ClientMVC/Startup.cs
@@ -1,9 +1,7 @@
-using IdentityServer4;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 
 namespace GCAP.ClientMVC
@@ -26,10 +24,6 @@ namespace GCAP.ClientMVC
                 .AddCookie("Cookies")
                 .AddOpenIdConnect("oidc", "GCAP(Great Coder Auth Provider)", options =>
                 {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-                    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
-                    options.SaveTokens = true;
-
                     options.Authority = "http://localhost:5000";
                     options.RequireHttpsMetadata = false;
 
@@ -37,11 +31,7 @@ namespace GCAP.ClientMVC
                     options.ClientSecret = "secret";
                     options.ResponseType = "code";
 
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        NameClaimType = "name",
-                        RoleClaimType = "role"
-                    };
+                    options.SaveTokens = true;
 
                     options.Scope.Add("api1");
                     options.Scope.Add("offline_access");
