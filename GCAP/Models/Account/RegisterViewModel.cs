a472117740d41a0975abfdea6da60394c30bc13e
Coder the Great <m_dehghan9112@outlook.com>
2020-03-26 16:37:45 +0430

    Adding Register endpoint to register new users...

---
+++ GCAP/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GCAP.Models.Account
+{
+    public class RegisterViewModel : LoginInputModel
+    {
+        [Required]
+        public string GivenName { get; set; }
+        [Required]
+        public string FamilyName { get; set; }
+    }
+}
